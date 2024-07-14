27ab3ce80e35e815978a471ca6ee6c246127f45a
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2024-07-14 19:17:00 +0530

    EsproAiMusic

--- EsproAiMusic/utils/inline/start.py
+++ EsproAiMusic/utils/inline/start.py
@@ -1,7 +1,7 @@
 from pyrogram.types import InlineKeyboardButton
 
 import config
-from EsproAiMusic import app
+from EsproMusic import app
 
 
 def start_panel(_):
@@ -25,11 +25,14 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
         ],
         [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
+        
+        
+        [InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID)],
     ]
     return buttons
