0ded883b9aaff39429509e55b8800c52c71778a2
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2024-07-16 19:57:43 +0530

    EsproAiMusic

--- EsproAiMusic/plugins/sudo/Shizu.py
+++ EsproAiMusic/plugins/sudo/Shizu.py
@@ -52,7 +52,7 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message(filters.command(["hiz","hizu"], prefixes=["s", "S"]) & admin_filter)
+@app.on_message(filters.command(["abu","aby"], prefixes=["b", "B"]) & admin_filter)
 async def restriction_app(app :app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
