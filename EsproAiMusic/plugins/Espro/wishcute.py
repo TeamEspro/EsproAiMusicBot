21dbf1b0ed3cb19a135fadb50793d9d51a2bb268
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2024-07-17 10:14:53 +0530

    EsproAiMusic

--- EsproAiMusic/plugins/Espro/wishcute.py
+++ EsproAiMusic/plugins/Espro/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from EsproAiMusic import app 
 
-SUPPORT_CHAT = "ALLTYPECC"
+SUPPORT_CHAT = "EsproSupport"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
