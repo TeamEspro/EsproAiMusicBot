794a1b584e4997307b25279137b7e25bfc33a772
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2024-08-19 09:23:56 +0530

    𝐄𝗌ρ𝗋ⱺ 𝐌ᥙsiᥴ [ ɴᴏ ᴀᴅʂ ] 🕊

--- EsproAiMusic/plugins/tools/gitinfo.py
+++ EsproAiMusic/plugins/tools/gitinfo.py
@@ -1,18 +1,11 @@
 import asyncio, os, time, aiohttp
 import aiohttp
 from pyrogram import filters
-from daxxhub import daxxhub as papadaxx
 from EsproAiMusic import app
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 ###
-@app.on_message(filters.command("daxxhub"))
-async def daxxhub(_, message):
-    text = message.text[len("/daxxhub") :]
-    papadaxx(f"{text}").save(f"daxxhub_{message.from_user.id}.png")
-    await message.reply_photo(f"daxxhub_{message.from_user.id}.png")
-    os.remove(f"daxxhub_{message.from_user.id}.png")
-####
+
 
 @app.on_message(filters.command(["github", "git"]))
 async def github(_, message):
