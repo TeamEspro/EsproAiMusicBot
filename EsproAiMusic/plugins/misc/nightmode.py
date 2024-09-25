8310542cfe77ebf99e4860b694bca62a8c0f35c4
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2024-09-25 22:45:07 +0530

    ***

--- EsproAiMusic/plugins/misc/nightmode.py
+++ EsproAiMusic/plugins/misc/nightmode.py
@@ -11,7 +11,6 @@ from EsproAiMusic.mongo.nightmodedb import nightdb,nightmode_on,nightmode_off,ge
 CLOSE_CHAT = ChatPermissions(
     can_send_messages=False,
     can_send_media_messages = False,
-    can_send_other_messages = False,
     can_send_polls = False,
     can_change_info = False,
     can_add_web_page_previews = False,
@@ -22,7 +21,6 @@ CLOSE_CHAT = ChatPermissions(
 OPEN_CHAT = ChatPermissions(
     can_send_messages=True,
     can_send_media_messages = True,
-    can_send_other_messages = True,
     can_send_polls = True,
     can_change_info = True,
     can_add_web_page_previews = True,
