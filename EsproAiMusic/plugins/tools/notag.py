38373f791ee68335eb6c19bfd1c92a62dbac4f8c
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2024-10-03 23:09:02 +0530

    ***

--- EsproAiMusic/plugins/tools/notag.py
+++ EsproAiMusic/plugins/tools/notag.py
@@ -4,40 +4,32 @@ from EsproAiMusic import app
 
 
 
-# List of approved user IDs who are allowed to tag
-approved_user_ids = set()
-
-# The username that no one should be able to tag without approval
-TARGET_USERNAME = "haiwanowner"  # Replace with the username that shouldn't be tagged
 
 
+# Usernames ko store karne ke liye ek list
+protected_usernames = [@HaiwanOwner]
 
-
-# Command to approve a user by Telegram ID
-@app.on_message(filters.command("approve") & filters.user("your_username"))
-def approve_user(client: Client, message: Message):
+# Command to add a username to the protected list
+@app.on_message(filters.command("adduser") & filters.user("your_username"))  # Replace with your admin username
+def add_user(client, message):
     if len(message.command) > 1:
-        try:
-            user_id = int(message.command[1])
-            approved_user_ids.add(user_id)
-            message.reply_text(f"User with ID {user_id} has been approved to tag @{TARGET_USERNAME}!")
-        except ValueError:
-            message.reply_text("Please provide a valid Telegram user ID!")
+        username = message.command[1].strip("@")  # Command format: /adduser @username
+        if username not in protected_usernames:
+            protected_usernames.append(username)
+            message.reply_text(f"Username @{username} ko successfully add kar diya gaya hai.")
+        else:
+            message.reply_text(f"Username @{username} pehle se list mein hai.")
     else:
-        message.reply_text("Please provide the Telegram user ID to approve.")
+        message.reply_text("Please ek username provide karein. Format: /adduser @username")
 
-
-# Monitor group messages and delete if someone tags the target username without approval
-@app.on_message(filters.group)
-def monitor_messages(client: Client, message: Message):
-    if message.entities:
-        for entity in message.entities:
-            if entity.type == "mention" and entity.user and entity.user.username == TARGET_USERNAME:
-                if message.from_user.id not in approved_user_ids:
-                    message.delete()
-                    client.send_message(
-                        message.chat.id, 
-                        f"Sorry {message.from_user.mention}, you're not allowed to tag @{TARGET_USERNAME}."
-                    )
-
+# Check if the mentioned username is in the protected list
+@app.on_message(filters.mentioned)
+def delete_message(client, message):
+    mentioned_usernames = [user.username for user in message.entities if user.type == "mention"]
+    
+    for username in mentioned_usernames:
+        if username in protected_usernames and message.from_user.username != username:
+            message.delete()  # Delete the message if username is in the protected list
+            print(f"Deleted message from {message.from_user.first_name} mentioning @{username}")
+            break
 
