80f1123a6b854360b44d3cc61b335bf3c64c1358
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2024-12-15 21:25:36 +0530

    ***

--- EsproAiMusic/plugins/misc/autoleave.py
+++ EsproAiMusic/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != 
-                                and i.chat.id != 
+                                and i.chat.id != -1002445654308
+                                and i.chat.id != -1002017775342
                             ):
                                 if left == 20:
                                     continue
