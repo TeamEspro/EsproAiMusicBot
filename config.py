9ddfa04f6a100a3386985d603f782f7486fa0f49
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2024-12-15 22:01:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", -1002422192977))
+LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from  on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 8007286147))
